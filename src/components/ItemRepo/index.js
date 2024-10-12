2bca415c2a1ae752750180dd9e94693cf8eaf751
Rafael Ferreira <rafaelsp20@hotmail.com>
2024-10-12 17:14:17 -0300

    Entrega do projeto wiki

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -12,7 +12,7 @@ function ItemRepo({repo, handleRemoveRepo}) {
     <ItemContainer onClick={handleRemove}>
         <h3>{repo.name}</h3>
         <p>{repo.full_name}</p>
-        <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
+        <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />       
         <a href="#"  rel="noreferrer" className="remover">Remover</a>
         <hr />
     </ItemContainer>
